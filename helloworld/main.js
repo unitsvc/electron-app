320be8accd4e13d774eed05acdf850268683992b
unitsvc <unitsvc@gmail.com>
2025-04-23 00:38:32 +0800

    add main.js

--- helloworld/main.js
+++ helloworld/main.js
@@ -5,11 +5,24 @@ const createWindow = () => {
         width: 800,
         height: 600,
         autoHideMenuBar: true,
-        alwaysOnTop: true
+        alwaysOnTop: true,
     })
     win.loadFile('index.html')
+    win.webContents.openDevTools();
 }
+
 
 app.whenReady().then(() => {
     createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
