6e2d2851561ae770e18ea3b9da7a565a08bdbbca
unitsvc <unitsvc@gmail.com>
2025-04-23 01:45:40 +0800

    package app

--- helloworld/docs/install.sh
+++ helloworld/docs/install.sh
@@ -4,8 +4,10 @@ pnpm config set registry https://registry.npmmirror.com
 
 pnpm config set electron_mirror https://cdn.npmmirror.com/binaries/electron/
 
+pnpm exec electron -v
+
 pnpm add nodemon -D
 
 pnpm add electron@latest -D
 
-pnpm exec electron -v
+pnpm add electron-builder -D
